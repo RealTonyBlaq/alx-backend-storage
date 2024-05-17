3e2034943ebd3c636beb77aeff3700c9127f7af5
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-17 23:00:52 +0100

    fix

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
 -- Script creates an SQL table users
 
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS user (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
     name VARCHAR(255),
-    country ENUM(`US`, `CO`, `TN`) NOT NULL DEFAULT=`US`
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 )
