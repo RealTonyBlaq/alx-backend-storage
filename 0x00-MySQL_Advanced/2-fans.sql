d0967f301d4a3535d42957a79fc3857bb6a46664
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-17 23:53:49 +0100

    order

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
 -- script that ranks country origins of bands,
 -- ordered by the number of (non-unique) fans
 
-SELECT origin, NOT UNIQUE(fans) AS nb_fans
+SELECT origin, NOT UNIQUE fans AS nb_fans
 FROM metal_bands;
