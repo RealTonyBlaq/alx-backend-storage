af34e7888bb9213338b59ffd9d292532556653a1
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-17 22:54:25 +0100

    added an enum()

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- Script creates an SQL table users
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255),
+    country ENUM(`US`, `CO`, `TN`) NOT NULL DEFAULT=`US`
+)
