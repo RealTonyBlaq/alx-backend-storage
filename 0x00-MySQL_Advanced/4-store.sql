a65980abe3c23d24730d1af570a5ca90c6e59f1c
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-19 07:45:42 +0100

    triggers

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -7,6 +7,8 @@ AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
     UPDATE items
-    SET quantity = items.quantity - NEW.number
+    SET quantity = quantity - NEW.number
     WHERE name = NEW.item_name;
 END$$
+
+DELIMITER ;
