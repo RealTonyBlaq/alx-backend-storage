6549f4e36c5888fe108859966db9b5f4e482c093
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-19 08:55:06 +0100

    procedures

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,3 +1,6 @@
 -- script that creates a stored procedure AddBonus that adds a new correction for a student.
 
-CREATE PROCEDURE AddBonus ( IN user_id INT, IN project_name VARCHAR(256), IN )
+DELIMITER $$
+
+CREATE PROCEDURE AddBonus ( IN user_id INT, IN project_name VARCHAR(256), IN score INT)
+BEGIN
