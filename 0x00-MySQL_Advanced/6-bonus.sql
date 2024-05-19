022e51e5d981663543df9e429bef7b428449e429
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-19 09:21:10 +0100

    procedures

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -7,9 +7,9 @@ BEGIN
     SELECT id INTO @my_project_id 
     FROM projects WHERE name = project_name;
     IF @my_project_id IS NULL THEN
-    INSERT INTO projects (name) VALUES (project_name);
-    SET @my_project_id = LAST_INSERT_ID();
-    
+        INSERT INTO projects (name) VALUES (project_name);
+        SET @my_project_id = LAST_INSERT_ID();
+    END IF;
     INSERT INTO corrections (user_id, project_id, score)
     VALUES (user_id, @my_project_id, score);
 END$$
