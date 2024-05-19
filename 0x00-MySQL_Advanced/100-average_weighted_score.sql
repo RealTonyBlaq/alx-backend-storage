f22393f5c8124abbb99015e7569759d326027232
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-20 00:57:14 +0100

    weighted average score

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,6 +1,8 @@
 -- script that creates a stored procedure ComputeAverageWeightedScoreForUser
 -- that computes and store the average weighted score for a student.
 
+DROP PROCEDURE ComputeAverageWeightedScoreForUser;
+
 DELIMITER $$
 
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser (IN u_id INT)
