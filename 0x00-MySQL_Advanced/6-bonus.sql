9718cbb8555eec9e97fdf09fc769b9baf52449d0
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-19 09:09:08 +0100

    procedures

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -5,4 +5,5 @@ DELIMITER $$
 CREATE PROCEDURE AddBonus ( IN user_id INT, IN project_name VARCHAR(256), IN score INT)
 BEGIN
     SET @my_project_id = (SELECT id FROM projects WHERE name = project_name);
-    INSERT INTO corrections (user_id, pro)
+    INSERT INTO corrections (user_id, project_id, score)
+    VALUES (user_id, )
