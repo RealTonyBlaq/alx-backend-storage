1b807adc70083c186d5b643ea9e388cc23c2e7dc
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-19 08:22:51 +0100

    update trigger

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -6,7 +6,7 @@ CREATE TRIGGER reset_attribute
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF NEW.email != OLD.email THEN
+    IF EXISTS NEW.email AND NEW.email != OLD.email THEN
         IF OLD.valid_email = 0 THEN
             SET valid_email = 1
         ELSE
