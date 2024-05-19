a66f337dd9c241293ede14102758a40e549cdb29
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-19 10:47:46 +0100

    average compute

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,6 @@
+-- script that creates a stored procedure ComputeAverageScoreForUser that computes
+-- and store the average score for a student. Note: An average score can be a decimal
+
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageScoreForUser ()
