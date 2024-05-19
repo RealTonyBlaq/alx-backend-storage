3b25dad6bc646be3650abd3d898286f1a7ff54be
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-20 00:43:28 +0100

    weighted average score

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -5,8 +5,8 @@ DELIMITER $$
 
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser (IN u_id INT)
 BEGIN
-    SELECT AVG(score * projects.weight)
+    SELECT AVG(score * projects.weight) INTO @wac
     FROM corrections
     INNER JOIN projects
     ON project_id = projects.id
-    
+    WHERE user_id = u_id;
