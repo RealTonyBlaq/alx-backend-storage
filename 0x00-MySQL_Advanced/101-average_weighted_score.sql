650ca57419d63d77fee4c42d25f86c2060f6e145
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-20 01:33:58 +0100

    all students procedure

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,6 +1,8 @@
 -- script that creates a stored procedure ComputeAverageWeightedScoreForUsers
 -- that computes and store the average weighted score for all students.
 
+DELIMITER $$
+
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers ()
 BEGIN
     FOR EACH ROW @u_id IN (SELECT id FROM users;)
@@ -8,10 +10,13 @@ BEGIN
         SELECT SUM(score * projects.weight / @wght) INTO @wasc
         FROM corrections
         RIGHT JOIN projects
-    ON corrections.project_id = projects.id
-    WHERE corrections.user_id = u_id;
+        ON corrections.project_id = projects.id
+        WHERE corrections.user_id = u_id;
 
-    -- Storing the weighted average to the user
-    UPDATE users
-    SET average_score = @wasc
-    WHERE id = u_id;
+        -- Storing the weighted average to the user
+        UPDATE users
+        SET average_score = @wasc
+        WHERE id = u_id;
+END$$
+
+DELIMITER ;
