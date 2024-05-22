07f759b87f9b1bd13a8de5d85c87083e4ae81c6c
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-22 23:09:24 +0100

    find school by topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+""" Using MongoDB """
+
+def schools_by_topic(mongo_collection, topic):
+    """ Returns the list of school having a specific topic """
+    mongo_collection.find()
