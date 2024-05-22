3783e62fa718e3a2303ac88e51442974a7509d9d
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-22 10:22:30 +0100

    list all

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 """ Using MongoDB """
 
-from pymongo import MongoClient
-
-
 def list_all(mongo_collection):
     """ function that lists all documents in a collection """
-    client = MongoClient()
+    
     
