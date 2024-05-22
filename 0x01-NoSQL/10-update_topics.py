96b50bec9d91ffea2c258356bafafd65e014c54c
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-22 22:33:21 +0100

    fix

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -3,7 +3,6 @@
 
 def update_topics(mongo_collection, name, topics):
     """ changes all topics of a school document based on the name """
-    for string in topics:
-        mongo_collection.update_many(
-            filter={'name': name},
-            update={'$push': {'topics': string}})
+    mongo_collection.update_many(
+        filter={'name': name},
+        update={'$set': {'topics': topics}})
