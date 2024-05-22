0240f0f8a9dfd2f22d8d8e50c507eb2a14468029
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-22 21:37:43 +0100

    update

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 """ Using MongoDB """
 
+from pymongo import MongoClient
+
+
 def update_topics(mongo_collection, name, topics):
     """ changes all topics of a school document based on the name """
     mongo_collection.update_many()
+    m
