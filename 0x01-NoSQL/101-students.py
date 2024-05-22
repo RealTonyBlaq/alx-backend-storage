8d8f3b637eaaf9f5378c37e7f0c3823ff3e17b00
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-23 00:46:10 +0100

    avg score

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -4,7 +4,8 @@
 def top_students(mongo_collection):
     """ Returns all students sorted by average score """
     students = mongo_collection.find()
-    
     for student in students:
         scores = [topic['score'] for topic in student['topics']]
         student['averageScore'] = sum(scores) / len(scores)
+
+    return sorted(students, )
