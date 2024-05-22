c7e496162949de713eed5033e3fa0e6ae0a40ca5
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-23 00:01:12 +0100

    nginx logs

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -12,6 +12,6 @@ print('{} logs'.format(logs.estimated_document_count()))
 print('Methods:')
 
 for method in methods:
-    print('\tmethod {}}: {}'.format(method, logs.count_documents(filter={'method': method})))
+    print('    method {}: {}'.format(method, logs.count_documents(filter={'method': method})))
 
 print('{} status check'.format(logs.count_documents(filter={'method': 'GET', 'path': '/status'})))
