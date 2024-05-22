13c82f98611d4a32da5d5987c2d1f77357d7e3ef
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-22 21:41:20 +0100

    update

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,7 +6,7 @@ from pymongo import MongoClient
 
 def update_topics(mongo_collection, name, topics):
     """ changes all topics of a school document based on the name """
-    mongo_collection.update_many({'name': name})
+    mongo_collection.update_many({'name': name}, )
     client = MongoClient()
     coll = client.my_db.school
     coll.update_many()
