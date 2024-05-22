00798dfced11bf05ade04dd3e84201cf3ed65449
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-23 00:58:46 +0100

    avg score

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -2,7 +2,7 @@
 """ Using MongoDB """
 
 def top_students(mongo_collection):
-    """ Returns all students sorted by average score """
+    """ Returns all students sorted by average score. """
     students = mongo_collection.find()
     for student in students:
         scores = [topic['score'] for topic in student['topics']]
