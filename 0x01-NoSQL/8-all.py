ffb38cdac7518fa565a49efd3b8623c3a2b1b504
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-22 10:26:47 +0100

    list all

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -2,7 +2,7 @@
 """ Using MongoDB """
 
 def list_all(mongo_collection):
-    """ function that lists all documents in a collection """
+    """ function that lists all documents in a collection. """
     result = mongo_collection.find()
     if not result:
         return []
