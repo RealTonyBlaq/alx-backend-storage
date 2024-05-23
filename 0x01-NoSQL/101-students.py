a8d74628799580d5b5a65f0fc1f1ac47c0d4cb1a
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-23 01:01:15 +0100

    avg score

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Using MongoDB """
 
+
 def top_students(mongo_collection):
     """ Returns all students sorted by average score """
     students = mongo_collection.find()
