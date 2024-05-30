4fe49f7a38a11344a1414db0bf05de387cad6cb8
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-05-30 09:52:05 +0100

    main

---
+++ 0x02-redis_basic/0-main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+import redis
+
+Cache = __import__('exercise').Cache
+
+cache = Cache()
+
+data = b"hello"
+key = cache.store(data)
+print(key)
+
+local_redis = redis.Redis()
+print(local_redis.get(key))
+
+print(cache.store.__annotations__)
