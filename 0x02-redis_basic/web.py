260bba363e04ac9f992820013590d08abc73c1d6
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-02 18:39:41 +0100

    webpage

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Get Page Module """
+
+import requests
+import redis
+
+
+def get_page(url: str) -> str:
+    """
+    The core of the function is very simple.
+    It uses the requests module to obtain the HTML content
+    of a particular URL and returns it.
+    """
+    
