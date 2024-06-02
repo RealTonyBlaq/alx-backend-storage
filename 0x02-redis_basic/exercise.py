504a60fa93f10aa165d7e282bacd00b9d4166001
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-02 18:05:10 +0100

    fixx

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -11,22 +11,19 @@ def replay(fn: Callable) -> None:
     """ Displays the history of calls of a particular function """
     r = redis.Redis()
     method = str(fn.__qualname__)
-    print(method)
     if r.exists(method) > 0:
         call_count = int(r.get(method))
         input_history = r.lrange(f'{method}:inputs', 0, -1)
         output_history = r.lrange(f'{method}:outputs', 0, -1)
         print(f'{method} was called {call_count} times:')
-        for input, output in zip(input_history, output_history):
+        for i in range(call_count):
             try:
-                input = input.decode('utf-8')
-            except Exception:
-                input = ""
-            try:
-                output = output.decode('utf-8')
-            except Exception:
-                output = ""
-            print(f"{method}(*{input}) -> {output}")
+                output = output_history[i].decode('utf-8')
+                input = int(input_history[i])
+            except ValueError:
+                input = input_history[i].decode('utf-8')
+            finally:
+                print(f'{method}(*{input}) -> {output}')
 
 
 def count_calls(f: Callable) -> Callable:
