16a7364e4f86c9d896e4f7881d2b85fb7311a37e
RealTonyBlaq <ifeanyiikpenyi@yahoo.com>
2024-06-02 16:59:55 +0100

    adding replay()

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -9,6 +9,7 @@ from functools import wraps
 
 def replay(fn: Callable) -> None:
     """ Displays the history of calls of a particular function """
+    redis
 
 
 def count_calls(f: Callable) -> Callable:
